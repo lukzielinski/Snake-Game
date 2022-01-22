960421bd1b68607b3ebd8a5f774fb272bd185717
ReQ <mrzielony22@gmail.com>
2022-01-22 17:39:11 +0100

    "Snake class added"

--- snake.js
+++ snake.js
@@ -1,5 +1,42 @@
 
+class Snake{
+    constructor(x, y, size){
+        this.x = x
+        this.y = y
+        this.size = size
+        this.tail = [{x:this.x, y:this.y}]
+        this.rotateX = 0
+        this.rotateY = 1
+    }
 
+    move(){
+        var newRect;
+        if(this.rotateX == 1){
+            newRect = {
+                x: this.tail[this.tail.length - 1].x + this.size,
+                y: this.tail[this.tail.length - 1].y
+            }
+        } else if(this.rotateX == -1){
+            newRect = {
+                x: this.tail[this.tail.length - 1].x - this.size,
+                y: this.tail[this.tail.length - 1].y
+            }
+        } else if(this.rotateY == 1){
+            newRect = {
+                x: this.tail[this.tail.length - 1].x,
+                y: this.tail[this.tail.length - 1].y + this.size
+            }
+        } else if(this.rotateX == -1){
+            newRect = {
+                x: this.tail[this.tail.length - 1].x,
+                y: this.tail[this.tail.length - 1].y - this.size
+            }
+        }
+
+        this.tail.shift()
+        this.tail.push(newRect)
+    }
+}
 
 
 var canvas = document.getElementById("canvas")
@@ -9,3 +46,16 @@ var snake = new Snake();
 var apple = new Apple();
 
 var canvasContext = canvas.getContext('2d');
+
+window.onload = ()=>{
+    gameLoop();
+}
+
+function gameLoop(){
+    setInterval(show, 1000/15)  //SPEED VALUE
+}
+
+function show(){
+    update();
+    draw();
+}
