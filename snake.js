ccdf7110390f596d8979e9e5498833c1b276cf27
ReQ <mrzielony22@gmail.com>
2022-01-22 20:08:51 +0100

    "Canvas Added"

--- snake.js
+++ snake.js
@@ -38,6 +38,27 @@ class Snake{
     }
 }
 
+class Apple{
+    constructor(){
+        var isTouching;
+        while(true){
+            isTouching = false;
+            this.x = Math.floor(Math.random() * canvas.width / snake.size) * snake.size
+            this.y = Math.floor(Math.random() * canvas.height / snake.size) * snake.size
+            for(var i = 0; i < snake.tail.length; i++){
+                if(this.x == snake.tail[i].x && this.y == snake.tail[i].y){
+                    isTouching = true
+                }
+            }
+            if(!isTouching){
+                break;
+            }
+            this.color = "pink"
+            this.size = snake.size
+        }
+    }
+}
+
 
 var canvas = document.getElementById("canvas")
 
@@ -59,3 +80,46 @@ function show(){
     update();
     draw();
 }
+
+function update(){
+    snake.move()
+
+}
+
+function draw(){
+    createRect(0,0,canvas.width, canvas.height, "black")
+    createRect(0,0, canvas.width, canvas.height)
+    for(var i = 0; i < snake.tail.length; i++){
+        createRect(snake.tail[i].x + 2.5, snake.tail[i].y + 2.5,
+            snake.size - 5, snake.size - 5, 'white')
+    }
+
+    canvasContext.font = "20px Arial"
+    canvasContext.fillStyle = "#00FF42"
+    canvasContext.fillText("Score: ", (snake.tail.length + 1),
+    canvas.width -120, 18);
+    createRect(apple.x, apple.y, apple.size, apple.size, apple.color)
+}
+
+function createRect(x,y, width, height, color){
+    canvasContext.fillStyle = color
+    canvasContext.fillRect(x,y, width, height)
+}
+
+window.addEventListener("keydown", (event)=>{
+    setTimeout(()=>{
+    if(event.keyCode == 37 &&snake.rotateX != 1){
+            snake.rotateX = -1
+            snake.rotateY = 0;
+        } else if(event.keyCode == 38 &&snake.rotateY != 1){
+            snake.rotateX = 0
+            snake.rotateY = -1;
+        } else if(event.keyCode == 39 &&snake.rotateX != -1){
+            snake.rotateX = 1
+            snake.rotateY = 0;
+        } else if(event.keyCode == 40 &&snake.rotateY != 1){
+            snake.rotateX = 0
+            snake.rotateY = 1;
+        }
+    }, 1)
+})
